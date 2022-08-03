ac781da7a7ccfa30aec3fec7540f8d3267c560f4
Savcinschi Egor <sasha_lagun1995@mail.ru>
2022-08-03 19:12:24 +0300

    fetch + button

---
+++ src/components/Button.tsx
@@ -0,0 +1,13 @@
+type ButtonPropsType = {
+    nickName:string
+    callback:()=>void
+}
+
+export const Button = (props:ButtonPropsType) => {
+    const onclickButtonHandler =()=> {
+        props.callback()
+    }
+    return(
+            <button onClick={onclickButtonHandler}>{props.nickName}</button>
+    )
+}
